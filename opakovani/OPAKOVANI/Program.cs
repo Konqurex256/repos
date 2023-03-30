4a887cb5ee8799ab60daa4c8a034b94a069578c8
Konqurex256 <misagal@email.cz>
2023-03-30 21:33:19 +0200

    poggers uz to mam zkoro hotovy

--- opakovani/OPAKOVANI/Program.cs
+++ opakovani/OPAKOVANI/Program.cs
@@ -14,6 +14,9 @@
             bool b = true; //promenna ktzera ma dve hodnoty, bud True nebo False
             double d = 1.1; //int promenna s moznosti pro destine cislo
             Convert.ToInt32 (d); //preveze na int hodnotu
+            // && //znamena "a" v podminkach
+            // || //znamena "nebo" v podminkach
+            // % //modus je specialni operat ktery vraci zbybajici hodnotu z deleni (10%4=2)
             if (d > i) //pokud
             {
 
@@ -43,8 +46,30 @@
                 Console.WriteLine(ii);
             }
 
+            switch (i) //if else if else na steroidech
+            {
+                case 0:
+                    Console.WriteLine(0);
+                    break;
+                case 1:
+                    Console.WriteLine(1);
+                    break;
+            }
+            
             Random random = new Random(); 
             int rand = random.Next(1, 10); //int rand prijme nahodnou hodnotu funkce random -> 1 az 10
+            Console.WriteLine($"{Math.PI}"); //umozuje psat v cw bez nutnosti + ; Math. funkce
+            // .Lenght() //delka daneho slova/stringu od nuly
+            // .StartsWith() //vypise cim dane slovo zacina
+            // .EndsWith() //vypise cim slovo konci
+            // .Contains("") //vypise zda slovo obsahuje dane pismeno/char a vypise True/False
+            // .IndexOf("") //vypise na jake pozici se dane pismeno /char nachazi -> od nuly
+            // .ToUpper() //slovo -> SLOVO
+            // .ToLower() //SLOVO -> slovo
+            int[] arr = {1, 2, 3 }; //promenna obsahujici vice hodnot stejneho druhu, ma pevnou delku
+            int[,] arr2d = { {1, 2 }, {3, 4 } }; //promenna obsahujici vice hodnot stejneho druhu, ma pevnou delku ale 2d
+            List<int> list = new List<int>(); //pole na steroidech, nema pevnou delku
+
 
             #endregion
 
