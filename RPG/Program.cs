0f332cd133773d22b7926b687551f3fc34c04ccc
Konqurex256 <misagal@email.cz>
2023-04-06 15:12:13 +0200

    idk maybe working

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -9,32 +9,24 @@ namespace GoblinAdventure
         static void Main(string[] args)
         {
             #region kecy okolo
-            Console.WriteLine("Ahoj, vítej v mé hře, toto je klasické RPG, jménem Goblin Advanture\n");
-            Thread.Sleep(500);
-            Console.WriteLine(".");
-            Thread.Sleep(500);
-            Console.WriteLine(".");
-            Thread.Sleep(500);
-            Console.WriteLine(".\n");
 
             Console.WriteLine($"Ahoj, vítej v Goblin Adventure!\n");
             Thread.Sleep(1000);
 
             Console.WriteLine("Pro začátek ti řeknu jak se tato hra hraje\n");
             Thread.Sleep(500);
-            Console.WriteLine("útok - ATTACK\nvyléčení - HEAL\n");
+            Console.WriteLine("útok - ATTACK\nvyléčení - HEAL\nblokovaní - BLOCK");
             Thread.Sleep(500);
-            Console.WriteLine("Prozatím tato hra bude mít 5 levelů (to je hodně na konzolovou aplikaci:>)");
-            Console.WriteLine("Tak jdeme na to ne? :D\n\n");
+            //Console.WriteLine("Prozatím tato hra bude mít 5 levelů (to je hodně na konzolovou aplikaci:>)");
+            //Console.WriteLine("Tak jdeme na to ne? :D\n\n");
             Thread.Sleep(500);
-            Console.Clear();
+            //Console.Clear();
             #endregion
-            int pzivoty = 50;
             Console.WriteLine("-LEVEL 1-");
-            Console.WriteLine("\"Šlimák\"");
+            Console.WriteLine("\"Šlimák\"\n");
             
-            Enemy Šlimák = new Enemy(100, 10, 15, "Šlimák"); //hp_boss prichozi_dmg odchozi_dmg name
-            php = 100;
+            Enemy Šlimák = new Enemy(100, 10, 15, 80, "Šlimák"); //hp_boss ; prichozi_dmg ; odchozi_dmg ; player_hp ; name
+            //int php = 100;
             Šlimák.start();
 
             #region for
@@ -108,8 +100,8 @@ namespace GoblinAdventure
             Console.WriteLine("damage + 10");
             Console.WriteLine("heal + 5");
 
-            Enemy Petr = new Enemy(300, 15, 25, "Petr"); //hp_boss prichozi_dmg odchozi_dmg name
-            Šlimák.start();
+            Enemy Petr = new Enemy(300, 15, 25, 100, "Petr"); //hp_boss ; prichozi_dmg ; odchozi_dmg ; player_hp ; name
+            Petr.start();
 
             #region for
             //for (int zivoty = 500; zivoty >= 0;)
@@ -175,7 +167,7 @@ namespace GoblinAdventure
             #endregion
 
             Console.WriteLine();
-            Console.WriteLine("\nHurá, porazil jsi Petra!\n");
+            Console.WriteLine("\n\nHurá, porazil jsi Petra!\n");
             Console.WriteLine("-LEVEL 3-");
             Console.WriteLine("\"\"");
             Console.WriteLine("damage + 20");
@@ -244,44 +236,51 @@ namespace GoblinAdventure
             //}
             #endregion
 
-            Enemy x = new Enemy(500, 25, 45, "x"); //hp_boss prichozi_dmg odchozi_dmg name
-            Šlimák.start();
+            Enemy x = new Enemy(500, 25, 45, 150, "x"); //hp_boss ; prichozi_dmg ; odchozi_dmg ; player_hp ; name
+            x.start();
         }
     }
+    
 
     class Enemy
     {
-        public Enemy(int hp, int pd, int dmg, string jmeno)
+        public Enemy(int hp, int pd, int dmg, int php, string jmeno)
         {
             this.hp = hp;
             this.jmeno = jmeno;
             this.pd = pd;
             this.dmg = dmg;
+            this.php = php;
         }
 
-        public int hp { get; private set; }
+        #region variables
         public string jmeno { get; private set; }
+        public int hp { get; private set; }
         public int pd { get; private set; }
         public int dmg { get; private set; }
-        public int php { get; set; }
+        public int php { get;  set; }
+        #endregion
 
         public void start()
         {
-            for (int zivoty = 200; zivoty >= 0;)
+            Console.WriteLine($"mas {php}hp");
+            for (int zivoty = hp; hp >= 0;)
             {
                 string akce = "";
                 Console.Write("Akce: ");
                 akce = Console.ReadLine();
 
+                Random random = new Random();
+                int PD = random.Next(1, 10);
 
                 switch (akce)
                 {
                     case "attack":
                         Console.WriteLine($"-{dmg} životů");
                         hp = (hp - dmg);
-                        if (zivoty > 0)
+                        if (hp > 0)
                         {
-                            Console.WriteLine(hp);
+                            Console.WriteLine($"{hp}hp");
                         }
                         else if (hp < 0)
                         {
@@ -292,24 +291,27 @@ namespace GoblinAdventure
                         break;
                     case "heal":
                         Console.WriteLine("+10 hp");
-                        hp = (hp + 10);
+                        php += 10;
                         Console.WriteLine(hp);
                         Thread.Sleep(500);
+                        break;
+                        case "block":
+                        PD = 0;
                         break;
                     default:
                         Console.Write("neplatná akce\n\n");
                         akce = Console.ReadLine();
                         break;
-
+                    
                 }
 
-                Random random = new Random();
-                int PD = random.Next(1, 10);
+                
 
                 switch (PD)
                 {
                     case 4:
                     case 6:
+                    case 8:
                     case 10:
                         php -= pd;
                         Console.WriteLine($"Oh ne, dostal si damage, ted mas {php} hp");
@@ -320,7 +322,6 @@ namespace GoblinAdventure
                     case 3:
                     case 5:
                     case 7:
-                    case 8:
                     case 9:
                         break;
                     default:
@@ -330,6 +331,21 @@ namespace GoblinAdventure
             }
         }
     }
+    #region Player
+    /*
+    class Player
+    {
+        public Player(string name, int php)
+        {
+            Name = name;
+            this.php = php;
+        }
+
+        public string Name { get; private set; }
+        public int php { get; set; }
+    }
+    */
+    #endregion
 }
 
 
